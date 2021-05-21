30274475f140e34736788308f4dc383d7a843765
Martin Lillepuu <martin@mlp.ee>
2021-05-21 20:59:36 +0300

    Add Brake_Status to carstate checks

--- selfdrive/car/subaru/carstate.py
+++ selfdrive/car/subaru/carstate.py
@@ -164,6 +164,7 @@ class CarState(CarStateBase):
         ("Dashlights", 10),
         ("BodyInfo", 10),
         ("CruiseControl", 20),
+        ("Brake_Status", 50),
       ]
 
     if CP.carFingerprint == CAR.FORESTER_PREGLOBAL:
